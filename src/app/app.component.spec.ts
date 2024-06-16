f541c1b0df3ae540360dc6d06df011bfb3705004
Paulo Guilherme <pauloguilherme@usp.br>
2024-06-16 17:42:45 -0300

    Fix project name
    Signed-off-by: Paulo Guilherme <pauloguilherme@usp.br>
    Co-authored-by: williamliu <wassabi.sashimi@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'one-ring-to-code-them-all'`, () => {
+  it(`should have as title 'Tic-Tac-Toe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('one-ring-to-code-them-all');
+    expect(app.title).toEqual('Tic-Tac-Toe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, one-ring-to-code-them-all');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Tic-Tac-Toe');
   });
 });
